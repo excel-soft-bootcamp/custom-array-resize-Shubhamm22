9f6dc5cbd58a3589e194a3134041425ab62cc574
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-05 14:43:41 +0530

    Add files via upload

---
+++ ArrayUltilityProgram/ArrayUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayUltilityProgram
+{
+    class ArrayUtility
+    {
+       
+       
+        public static void Resize(ref int[] array, int newSize)
+        {           
+            int[] temp = new int[newSize];
+            Array.Copy(array, temp, array.Length);
+            array = temp; 
+
+        }
+    }
+}
